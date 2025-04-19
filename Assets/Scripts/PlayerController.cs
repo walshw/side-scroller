63e3ddfc0aec230667b4d53c1acd30915e494b33
walshw <williamdouglaswalsh@gmail.com>
2025-04-18 20:35:06 -0400

    Added sprite flipping, added head and attacking to player, renamed scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -11,9 +12,16 @@ public class PlayerController : MonoBehaviour
     private Collider2D cl;
     private bool airborne;
     private bool jumping;
+    private float xVel;
+    private float yVel;
+    private bool facingRight;
+    private float jumpForce = 5f;
 
     void Start()
     {
+        // MY GAME RULE: By default, every sprite should be facing right, if you want it to look left then flip x
+        facingRight = transform.localScale.x >= 0;
+
         rb = GetComponent<Rigidbody2D>();
         cl = GetComponentInChildren<Collider2D>();
     }
@@ -22,36 +30,68 @@ public class PlayerController : MonoBehaviour
     {
         GroundCheck();
         ProcessInputs();
+        HandleTurningSprite();
     }
 
     void FixedUpdate()
     {
         ApplyMovement();
     }
-    
-    void GroundCheck() {
+
+    void GroundCheck()
+    {
         Vector2 bottom = new Vector2(transform.position.x, transform.position.y - cl.bounds.extents.y);
         Debug.DrawRay(bottom, -transform.up * rayLength, Color.red, 1f);
         airborne = !Physics2D.Raycast(bottom, -transform.up, rayLength);
     }
 
-    void ProcessInputs() {
+    void ProcessInputs()
+    {
+        xVel = Input.GetAxis("Horizontal");
+
         if (Input.GetKeyDown(KeyCode.Space) && !airborne)
         {
             jumping = true;
         }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("d");
+            Debug.DrawRay(transform.position, GetForward() * 3f, Color.green, 1f);
+        }
     }
 
-    void ApplyMovement() {
-
-        float yVel = rb.velocity.y;
+    void ApplyMovement()
+    {
+        yVel = rb.velocity.y;
 
         if (jumping)
         {
             jumping = false;
-            yVel += 5f;
+            yVel += jumpForce;
         }
 
-        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, yVel);
+        rb.velocity = new Vector3(xVel * speed, yVel);
+    }
+
+    void HandleTurningSprite()
+    {
+        if (xVel < 0)
+        {
+            facingRight = false;
+            transform.localScale = new Vector3(-Math.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            return;
+        }
+
+        if (xVel > 0)
+        {
+            facingRight = true;
+            transform.localScale = new Vector3(Math.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            return;
+        }
+    }
+
+    Vector3 GetForward() {
+        return facingRight ? transform.right : -transform.right;
     }
 }
